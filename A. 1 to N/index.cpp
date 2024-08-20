d00652f5692a9f123bdf7548c2c95f769ccf88bf
Abdullah <112831356+Vega47@users.noreply.github.com>
2024-08-20 17:11:02 +0300

    Problme B.1 Loops

--- A. 1 to N/index.cpp
+++ A. 1 to N/index.cpp
@@ -1,11 +1,9 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
-{
+int main(){
     int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        cout << i << endl;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<i<<endl;
     }
 }
