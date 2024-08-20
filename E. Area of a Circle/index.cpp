9c358ecb380021c24b6c469fb0689006173004fe
Abdullah <112831356+Vega47@users.noreply.github.com>
2024-08-20 16:58:37 +0300

    Problme E

---
+++ E. Area of a Circle/index.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    double r;
+    cin>>r;
+    cout << fixed << setprecision(9);
+    cout<<(r*r)*3.141592653;
+    
+}
+
+
+
+
