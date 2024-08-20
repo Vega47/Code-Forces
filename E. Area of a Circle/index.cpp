59957ca5c30f3186ae651340ec8593a95b732b2a
Abdullah <112831356+Vega47@users.noreply.github.com>
2024-08-20 17:02:00 +0300

    Problme A.1 Loops

--- E. Area of a Circle/index.cpp
+++ E. Area of a Circle/index.cpp
@@ -1,15 +1,10 @@
-#include<iostream>
-#include<iomanip>
+#include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
     double r;
-    cin>>r;
+    cin >> r;
     cout << fixed << setprecision(9);
-    cout<<(r*r)*3.141592653;
-    
+    cout << (r * r) * 3.141592653;
 }
-
-
-
-
