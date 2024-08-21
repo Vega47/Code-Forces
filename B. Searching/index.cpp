622d9e415a183d2f2193f61bfd667b0b576edc67
Abdullah <112831356+Vega47@users.noreply.github.com>
2024-08-21 13:44:19 +0300

    Searching

---
+++ B. Searching/index.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    int arr[t];
+    for(int i=0;i<t;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+    bool flag=false;
+    int index;
+    for(int i=0;i<t;i++){
+        if(arr[i]==target){
+            flag=true;
+            index=i;
+            break;
+        }
+    }
+    if(flag){
+        cout<<index<<endl;
+    }else{
+        cout<< -1;
+    }
+}
