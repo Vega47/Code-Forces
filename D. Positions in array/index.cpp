95ff15018e6d747809dec0eab278c93e5ae5e6c8
Abdullah <112831356+Vega47@users.noreply.github.com>
2024-08-21 14:47:03 +0300

    Postions

---
+++ D. Positions in array/index.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    int arr[t];
+    for(int i=0;i<t;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<t;i++){
+        if(arr[i]<=10){
+            cout<<"A["<<i<<"]"<<" = "<<arr[i]<<endl;
+        }
+    }
+}
