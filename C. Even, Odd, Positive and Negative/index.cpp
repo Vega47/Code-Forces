079e45844048ea7a46637c7e1c04036577cf5716
Abdullah <112831356+Vega47@users.noreply.github.com>
2024-08-21 12:10:13 +0300

    Update index.cpp

--- C. Even, Odd, Positive and Negative/index.cpp
+++ C. Even, Odd, Positive and Negative/index.cpp
@@ -1,27 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int t;
-    cin>>t;
+    cin >> t;
     int x;
-    int odd=0,even=0,nga=0,pos=0;
-    while(t--){
-        cin>>x;
-        if(x%2==0){
+    int odd = 0, even = 0, nga = 0, pos = 0;
+    while (t--)
+    {
+        cin >> x;
+        if (x % 2 == 0)
+        {
             even++;
         }
-        if(x%2!=0){
+        if (x % 2 != 0)
+        {
             odd++;
         }
-        if(x>0){
+        if (x > 0)
+        {
             pos++;
         }
-        if(x<0){
+        if (x < 0)
+        {
             nga++;
         }
     }
-    cout<<"Even: "<<even<<endl;
-    cout<<"Odd: "<<odd<<endl;
-    cout<<"Postive: "<<pos<<endl;
-    cout<<"Negative: "<<nga<<endl;
+    cout << "Even: " << even << endl;
+    cout << "Odd: " << odd << endl;
+    cout << "Positive: " << pos << endl;
+    cout << "Negative: " << nga << endl;
 }
