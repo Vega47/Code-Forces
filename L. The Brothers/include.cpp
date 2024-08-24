0622be793656f573c69314a8ff24f1037aae213f
Abdullah <112831356+Vega47@users.noreply.github.com>
2024-08-24 12:22:18 +0300

    h

---
+++ L. The Brothers/include.cpp
@@ -0,0 +1,16 @@
+#include<string>
+#include<iostream>
+using namespace std;
+int main(){
+    string f1,f2,l1,l2;
+    cin>>f1>>l1;
+    cin>>f2>>l2;
+    if(l1==l2){
+        cout<<"ARE Brothers";
+    }else{
+        cout<<"NOT";
+    }
+    // string fl1=f1+l1;
+    // string fl2=f2+l2;
+
+}
